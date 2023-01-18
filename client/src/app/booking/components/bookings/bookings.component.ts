aa01fbf6eeadb2f936e72b60c8848ab75744cb0f
Your Name <you@example.com>
2023-01-18 13:26:57 +0300

    Подсветил бронь если конец аренды

--- client/src/app/booking/components/bookings/bookings.component.ts
+++ client/src/app/booking/components/bookings/bookings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { DatePipe } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { CarsService } from 'src/app/cars/services/cars.service';
 import { ClientsService } from 'src/app/clients/services/clients.service';
@@ -18,6 +18,7 @@ import { BookingsService } from '../../services/bookings.service';
   selector: 'app-bookings',
   templateUrl: './bookings.component.html',
   styleUrls: ['./bookings.component.css'],
+  providers: [DatePipe]
 })
 export class BookingsComponent implements OnInit, OnDestroy {
   
@@ -29,17 +30,23 @@ export class BookingsComponent implements OnInit, OnDestroy {
   limit: any = STEP;
   loading = false;
   noMoreCars: Boolean = false;
+  todayDate: any = new Date().toDateString();
+  todayDateFormat = this.datePipe.transform(this.todayDate, 'yyyy-MM-dd');
 
   constructor(
     private bookings: BookingsService,
     private router: Router,
     private rote: ActivatedRoute,
     private cars: CarsService,
-    private clients: ClientsService
+    private clients: ClientsService,
+    private datePipe: DatePipe
   ) {}
 
   ngOnInit(): void {
     this.fetch();
+
+    
+    
   }
 
   ngOnDestroy(): void {
@@ -71,32 +78,6 @@ export class BookingsComponent implements OnInit, OnDestroy {
       this.loading = false;
       this.xsbookings = this.xsbookings.concat(bookings);
 
-      // of(this.xsbookings)
-      //   .pipe(
-      //     map((bookings) => {
-      //       bookings.forEach((xsbooking) => {
-      //         this.cars.getById(xsbooking.car).subscribe((data) => {
-      //           xsbooking.car_meta = data;
-      //         });
-      //       });
-
-      //       return bookings;
-      //     })
-      //   )
-      //   .pipe(
-      //     map((bookings) => {
-      //       bookings.forEach((xsbooking) => {
-      //         this.clients.getById(xsbooking.client).subscribe((data) => {
-      //           xsbooking.client_meta = data;
-      //         });
-      //       });
-
-      //       return bookings;
-      //     })
-      //   )
-      //   .subscribe((data) => {
-      //     console.log('После подписки', this.xsbookings);
-      //   });
 
     });
   }
